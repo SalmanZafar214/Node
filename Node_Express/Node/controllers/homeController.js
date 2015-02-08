e0ef04023cc556d42f595badeac5b748da5c2403
SalmanZafar214 <salman.zafar.214@googlemail.com>
2015-02-09 00:26:24 +0500

    porject updated

--- Node_Express/Node/controllers/homeController.js
+++ Node_Express/Node/controllers/homeController.js
@@ -9,6 +9,7 @@
             
             data.getNoteCategories(function (err, results) {
                 res.render('index', { title: "Node using Express and Vash", error: err, categories: results });
+                req.session.userName = 'Anonymous';
             })
         });
         
@@ -17,18 +18,38 @@
             res.send('About Page');
         });
         
-        app.get('/Fortune', function (req, res) {
+        app.get('/Fortune', function (request, res) {
             data.getFortune(function (err, results) {
                 res.render('fortune', {
                     title: "Node using Express and Vash", 
                     fortune: results, 
                     error: err,
-                    query: req.query.style,
-                    cookie: req.cookie,
-                    session: req.session,
+                    query: request.query.style,
+                    cookie: request.cookie,
+                    session: request.session.userName,
                 });
             })
         });
+        
+        
+        app.post('/process', function (req, res) {
+            console.log('Form (from querystring): ' + req.query.form);
+            console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+            console.log('Name (from visible form field): ' + req.body.name);
+            console.log('Email (from visible form field): ' + req.body.email);
+            res.redirect(303, '/fortune'); //redirecting with status code
+
+            req.session.userName = 'Anonymous';
+
+            //if (req.xhr || req.accepts('json,html') === 'json') {
+            //    // if there were an error, we would send { error: 'error description' }
+            //    res.send({ success: true });
+            //} else {
+            //    // if there were an error, we would redirect to an error page
+            //    res.redirect(303, '/thank-you');
+            //}
+        });
+        
         
         
         // custom 404 page
--- Node_Express/Node/server.js
+++ Node_Express/Node/server.js
@@ -5,7 +5,20 @@ var express = require('express'); //adding express dependency
 var app = express(); //as its a singelton
 app.set('view engine', 'vash');//setting up view engine
 app.set('port', process.env.PORT || 3000); //setting up a port 
+app.use(require('body-parser')()); // to enable req.body
 app.use(express.static(__dirname + '/public'));//loading static resources from public folder
+
+app.use(require('cookie-parser')());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    key: 'express.sid',
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}));
+
+
+
 
 var controllers = require('./controllers');
 controllers.init(app);//Mapping the routes
@@ -17,3 +30,19 @@ app.listen(app.get('port'), function () {
     console.log('Express started on http://localhost:' +
     app.get('port') + '; press Ctrl-C to terminate.');
 });
+
+app.use(function (req, res, next) {
+    var views = req.session.views
+    
+    if (!views) {
+        views = req.session.views = {}
+    }
+    
+    // get the url pathname 
+    var pathname = parseurl(req).pathname
+    
+    // count the views 
+    views[pathname] = (views[pathname] || 0) + 1
+    
+    next()
+})
